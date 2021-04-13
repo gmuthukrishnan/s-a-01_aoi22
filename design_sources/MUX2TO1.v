782ba41dffde7cf7609bf19b93e8831d735950ac
Muthukrishnan G <63191706+gmuthukrishnan@users.noreply.github.com>
2021-04-14 03:49:54 +0530

    2 : 1 MUX module for instantiation

--- design_sources/MUX2TO1.v
+++ design_sources/MUX2TO1.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/17/2020 05:27:51 PM
-// Design Name: 
-// Module Name: MUX2TO1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 module mux2to1(y,sel,I0,I1);
 output y;
 input sel,I0,I1;
